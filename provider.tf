31d7662da1af0cb09d111fe45be74e2a25704b47
Karan Goyal <karangoyal134@gmail.com>
2026-07-01 16:44:05 +0000

    corrected the provider

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     use_lockfile = true
-    key          = "/prod/terraform.tfstate"
+    key          = "prod/terraform.tfstate"
     region       = "us-east-1"
   }
   required_providers {
